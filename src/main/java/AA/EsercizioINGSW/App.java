4e1a4bcd624f2b3c6e86a09fa0643350be1e6b5c
Giuseppe <Giuseppe@MSI>
2020-10-28 11:43:28 +0100

    Primo

---
+++ src/main/java/AA/EsercizioINGSW/App.java
@@ -0,0 +1,12 @@
+package AA.EsercizioINGSW;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Math math = new Math();
+		System.out.println(math.primeNumersGenerator(20));
+	}
+}
---
+++ src/main/java/AA/EsercizioINGSW/Math.java
@@ -0,0 +1,33 @@
+package AA.EsercizioINGSW;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Math {
+	public List<Integer> primeNumersGenerator(int n) {
+		List<Integer> primeNumbers = new LinkedList<Integer>();
+		if (n >= 2) {
+			primeNumbers.add(2);
+		}
+		for (int i = 0; i <= n; i += 2) {
+			if (isPrime(i)) {
+				primeNumbers.add(i);
+			}
+		}
+		return primeNumbers;
+	}
+
+	public boolean isPrime(int number) {
+		if (number < 2) {
+			return false;
+		}
+		for (int i = 2; i * i < number; i++) {
+			if (number % i == 0) {
+				return false;
+			}
+
+		}
+		return true;
+	}
+
+}
